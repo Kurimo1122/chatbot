e5d7b73a354c011866a9a9a502cf2b6f3627c891
Kurimo1122 <kugimoto1122@gmail.com>
2016-11-27 16:56:50 +0900

    Check errors

--- app.py
+++ app.py
@@ -73,7 +73,8 @@ def callback():
         if isinstance(event, MessageEvent):    
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=event.message.text)
+                #TextSendMessage(text=event.message.text)
+                TextSendMessage(details[].message.text)
                 #TextSendMessage(text="aiueo")
             )
 
